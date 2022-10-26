53443a0ea26f8cb65ad2f3474c898e70f78dacef
ExplosiveEnd <henockdinberu11@gmail.com>
2022-10-26 18:58:16 -0400

    Recreated problemGenerator

--- main.py
+++ main.py
@@ -1,56 +1,32 @@
-import discord, random
-# from discord.ext import commands
+import discord, random, os, sys
+from env import DISCORD_TOKEN
 
 client = discord.Client(intents=discord.Intents.default())
 
-# client = commands.Bot(command_prefix="!", intents=discord.Intents.all())
-
-# @bot.start("MTAzMzU0ODQwMzc5NzQxMzkyOA.Gk4bZv.6dFlHzq4nvZsgEFOQqrS7_ZpL55u7vnjSBbJqw")
+TOKEN = os.getenv(DISCORD_TOKEN)
 
 arr = []
 global y, add, test, x, ans
 y = random.randint(1,5)
 add = random.randint(1, 5)
+test = False
 x = random.randint(1,5)
-
-def betterProblemGenerator():
-    eq = []
-    op = ["+", "-", "*", "/"]
-    numOfValues = random.randint(1,4)
-    numOfOperations = numOfValues-1
-
 
 
 def problemGenerator():
-    opp = ["+", "-", "*", "/"]
-    ran1 = random.randint(0,99)
-    ran2 = random.randint(0, 99)
-    ran0 = random.randint(0,3)
-    if ran0 == 0:
-        ans = ran1 + ran2
-    elif ran0 == 1:
-        ans = ran1 - ran2
-    elif ran0 == 2:
-        ans = ran1 * ran2
-    else:
-        ans = ran1 / ran2
-    return str(ran1) + " " + opp[ran0] + " " + str(ran2)
-    # eq = []
-    # for i in range(numOfValues + numOfOperations):
-    #     if not i % 2:
-    #         eq.append(values[i/2])
-    #         ranX = random.randint(0,1)
-    #         if not i and ranX:
-    #             eq[0] += 'x'
-    #     elif difficulty < 2:
-    #         if i == 1:
-    #             eq.append(operations[0])
-    #         else:
-    #             randOp = random.randint(1, difficulty + 3)
-    #             eq.append(operations[randOp])
-    # e = ''.join(eq)
-    # return e
-
+    eq = []
+    options = ["+", "-", "*", "/"]
+    numOfValues = random.randint(2,4)
+    numOfOperations = numOfValues-1
+    values = [str(random.randint(1,100)) for i in range(numOfValues)]
+    operations = [random.randint(0,3) for i in range(numOfOperations)]
+    for i in range(numOfValues):
+        if i == numOfValues-1:
+            eq.append(values[i])
+        else:
+            eq.append(values[i])
+            eq.append(options[operations[i]])
+    return " ".join(eq) + " = "
 
 @client.event
 async def on_ready():
@@ -68,15 +44,8 @@ async def on_message(message):
     if y == x:
         await message.channel.send(problemGenerator())
         test = True
-        await message.reply("GOD DID")
-        if str(ans) == message.author:
-            await message.channel.send("Correct")
-        else:
-            await message.channel.send("Wrong")
     else:
         y = random.randint(1,5)
-
-
 
 
 def rand(start: int):
